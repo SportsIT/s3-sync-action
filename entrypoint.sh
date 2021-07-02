d25d6d7a0a1dbbf1101c91c486e080921bad6dd5
Trevor Olson <teolson17@gmail.com>
2021-07-02 14:31:08 -0700

    Added invalidation to cloudfront

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,11 @@ if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
+#  If no path to invalidate, then default it
+if [ -n "$INVALIDATION_PATH" ]; then
+  INVALIDATION_PATH="/*"
+fi
+
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
@@ -43,6 +48,11 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
+
+if [ - "$DISTRIBUTION_ID" ]; then
+  # Invalid the cloundfront distribution
+  sh -c "aws cloudfront create-invalidation --distribution-id ${DISTRIBUTION_ID} --paths "${$INVALIDATION_PATH}" --profile s3-sync-action"
+fi
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
